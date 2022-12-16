1550921153ee35832cc0b967339388abdf123097
admin <admin@IT-Akademy-20.local>
2022-12-16 16:59:23 +0100

    exo5

--- exo5.php
+++ exo5.php
@@ -4,21 +4,44 @@
 //    Jeu des bâtonnets
 // --------------------------
 
-// Lorsqu'il reste 4 bâtonnets, il faut que ça soit le tour de l'ordi
+// The player who takes the last sticks wins
 
-$sticks = 10;
+// Number of sticks at the start
+$sticks = 20;
 
+// While there are sticks
 while ($sticks > 0) {
-    $stickTakenPlayer = readline("\nSaisissez un nombre de bâtonnets à prendre : ");
-    $sticks -= $stickTakenPlayer;
+    // Show how much sticks they are
+    echo "Il reste $sticks bâtonnets.\n";
 
-    if ($sticks > 4) {
-        $stickTakenComputer = rand(1, 3);
-        $sticks -= $stickTakenComputer;
-        echo "\nL'ordi a pri : " . $stickTakenComputer . " bâtonnets";
+    // Turn of player 1
+    $player1 = getPlayerMove();
+    $sticks -= $player1;
+    if ($sticks <= 0) {
+        echo "Le joueur 1 a gagné !\n";
+        break;
     }
 
-    echo $sticks % 4;
+    // Turn of player 2
+    $player2 = getPlayerMove();
+    $sticks -= $player2;
+    if ($sticks <= 0) {
+        echo "Le joueur 2 a gagné !\n";
+        break;
+    }
+}
 
-    echo "\nNombre de batônnets restants : " . $sticks;
+// Function to get the number of sticks took from a player
+function getPlayerMove()
+{
+    // Ask to the player how many sticks he wants to take (between 1 and 3)
+    $move = readline("Combien de bâtonnets voulez-vous prendre ? ");
+
+    //Check if the input is valid
+    if ($move < 1 || $move > 3) {
+        echo "Vous devez prendre entre 1 et 3 bâtonnets.\n";
+        return getPlayerMove();
+    } else {
+        return $move;
+    }
 }
